0140822929f0c8207685ba8ba62a8f999fae5dc8
Sean Kidwell <seankidwell@Seans-MacBook-Pro.local>
2018-06-26 16:42:18 -0600

    done

--- server/index.js
+++ server/index.js
@@ -2,16 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
 const port = 1337;
+const session = require('express-session');
+require('dotenv').config();
+
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 const app = express();
 
 app.use( bodyParser.json() );
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 10000
+  }
+}))
+
+app.use(createInitialSession);
 app.use( express.static( `${__dirname}/../build` ) );
 
-app.post( "/api/messages", mc.create );
+// app.use((req, res, next) => {
+//   let {method} = req;
+//   if (method === 'PUT' || method === 'POST') {
+//     filter(req, res, next);
+//   } else {
+//     next();
+//   }
+// })
+
+app.post( "/api/messages", filter, mc.create );
 app.get( "/api/messages", mc.read );
-app.put( "/api/messages", mc.update );
+app.put( "/api/messages", filter, mc.update );
 app.delete( "/api/messages", mc.delete );
+app.get('/api/messages/history', mc.history);
 
 
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
---
+++ server/middlewares/filter.js
@@ -0,0 +1,9 @@
+let notAllowed = ["fick", "chud"];
+
+module.exports = (req, res, next) => {
+  while (notAllowed.find(word => req.body.text.includes(word))) {
+    const badWord = notAllowed.find(word => req.body.text.includes(word));
+    req.body.text = req.body.text.replace(badWord, "*".repeat(badWord.length));
+  }
+  next();
+};
